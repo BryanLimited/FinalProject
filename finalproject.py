c71f8396dc38ad0814780ed742158b8d5fde2011
Bryan <155593579+BryanLimited@users.noreply.github.com>
2024-09-18 21:06:09 +0000

    Pygame Library Setup  w/ Window Screen

--- finalproject.py
+++ finalproject.py
@@ -1,1 +1,10 @@
-print("hello")
+#Authors: Bryan Soares & Elijah Avri
+
+
+#Importing pygame
+import pygame
+
+#Window screen setup 
+pygame.init()
+WindowScreen = pygame.display.set_mode(1980,1080)
+running = True
