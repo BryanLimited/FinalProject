bad379d4899fbf67215acc2fe4877c5c03bdd2b2
Bryan <155593579+BryanLimited@users.noreply.github.com>
2024-09-20 04:23:25 +0000

    Changes to window display

--- finalproject.py
+++ finalproject.py
@@ -9,5 +9,13 @@ pygame.init()
 
 #Window screen setup 
 WindowScreen = pygame.display.set_mode((1980,1080))
+pygame.display.set_caption("Bootleg Flappy")
+
+
+
 running = True
+while running:
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      running = False
 
