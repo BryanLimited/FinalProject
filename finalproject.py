9db5827d17ff88c431b30e56e56834bf9f3a968e
Bryan <155593579+BryanLimited@users.noreply.github.com>
2024-09-24 18:49:09 +0000

    Fixed directory calling again... I think

--- finalproject.py
+++ finalproject.py
@@ -9,19 +9,20 @@ import pygame
 pygame.init()
 
 #setting root path for Python 
-os.chdir("C:/Users")
-
-
+os.chdir("C:\Users")
 RootPath = os.path.join(os.path.dirname(os.path.realpath(__file__).parent))
+
+
 #Window screen setup 
 WindowScreen = pygame.display.set_mode((480,720))
-pygame.display.set_caption("Bootleg Flappy")
+pygame.display.set_caption("Final project - Flappy Bird")
 
 
 #Background Image Setup
 Background = pygame.image.load(os.path.join(RootPath,'assets', 'SkyAsset.png'))
 
-
+#Setting mouse cursor
+pygame.mouse.set_cursor(*pygame.cursors.arrow)
 
 running = True
 while running:
@@ -32,5 +33,4 @@ while running:
 
       
       pygame.display.update()
-
 
