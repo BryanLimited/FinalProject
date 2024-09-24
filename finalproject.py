35d5d21de2cbaba5ae4a906633a66462160ee058
Bryan <155593579+BryanLimited@users.noreply.github.com>
2024-09-24 21:25:21 +0000

    wowzers

--- finalproject.py
+++ finalproject.py
@@ -8,34 +8,27 @@ import os
 import pygame
 pygame.init()
 
-#setting root path for Python 
-home_directory = os.path.expanduser("~")
+#setting root path
 
+RootPath = os.path.dirname(__file__)
 
 #Window screen setup 
-WindowScreen = pygame.display.set_mode((480,720))
-pygame.display.set_caption("Final project - Flappy Bird")
 
-print("Current Working Directory:", os.getcwd())
+WindowScreen = pygame.display.set_mode((480,720))
+pygame.display.set_caption("Bootleg Flappy")
+
 
 #Background Image Setup
-Background = pygame.image.load(os.path.join(home_directory, 'Downloads','FinalProject-main.zip', 'FinalProject-main', 'assets', 'SkyAsset.png'))
-#Example to get the lazy way -- -- -- -- C:\Users\Bryan\Downloads\FinalProject-main.zip\FinalProject-main\assets
+LoadImage = os.path.join(RootPath, 'assets' , 'SkyAsset.png')
+Background = pygame.image.load(LoadImage)
 
 
-
-
-
-#Setting mouse cursor
-pygame.mouse.set_cursor(*pygame.cursors.arrow)
 
 running = True
 while running:
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       running = False
-      
-
-      
       pygame.display.update()
+
 
