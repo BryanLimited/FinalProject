460d1783fa839884904d734c2bda9fa7098fed9f
Bryan <155593579+BryanLimited@users.noreply.github.com>
2024-10-09 20:56:13 +0000

    Base floor position and speed added

--- finalproject.py
+++ finalproject.py
@@ -1,55 +1,55 @@
 #Authors: Bryan Soares & Elijah Avri
 
-
 import os
 import sys
 from pathlib import Path
-
-
-#Importing pygame
 import pygame
 pygame.init()
 
-#Calling path 
 FolderPath = os.path.abspath(sys.argv[0])
 RootPath = os.path.dirname(FolderPath)
-
-#Window screen setup 
 
 WindowScreen = pygame.display.set_mode((480,720))
 pygame.display.set_caption("Flappy Bird")
 
 
-#- - - - - - - - - - - - - - - -#Image Setup - -- - - - - - - - - - - - - - - - - - - -
+# - - - - - - - - - - - -Image Class Setup - - - - - - - - - - - - - - - - - - - - -
 
-#Background
 LoadImage = os.path.join(RootPath, 'assets' , 'SkyAsset.png')
 Background = pygame.image.load(LoadImage)
 
-#Player
 LoadPLayer = os.path.join(RootPath, 'assets', 'PlayerBird.png')
 Player = pygame.image.load(LoadPLayer)
 
-#Pipe 
 PipeImageLoad = os.path.join(RootPath, 'assets', 'Pipe.png')
 PipeImage = pygame.image.load(PipeImageLoad)
 
-#Base
 BaseImageLoad = os.path.join(RootPath, 'assets', 'base.png')
-BaseImage = pygame.image.load(BaseImageLoad)
+BaseFloor = pygame.image.load(BaseImageLoad)
 
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+#- - - - - - - - - - - - -  - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 
+
+
+# - - - -Foor base position and speeed - - -
+base_speed = 2
+base_x = 0
+#- - - - - - - - - - - - - - - - - - - - - -
 
 
 running = True
 while running:
-  print("Running game")
-  for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-      running = False
+    print("Running game")
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    base_x -= base_speed
+    if base_x <= -WindowScreen.get_width():
+        base_x = 0
+
     pygame.display.update()
-    WindowScreen.blit(Background, (0,0))
-    WindowScreen.blit(BaseImage, 1,1)
-      
+    WindowScreen.blit(Background, (0, 0))
+    WindowScreen.blit(BaseFloor, (base_x, WindowScreen.get_height() - BaseFloor.get_height()))
+    WindowScreen.blit(BaseFloor, (base_x + WindowScreen.get_width(), WindowScreen.get_height() - BaseFloor.get_height()))
