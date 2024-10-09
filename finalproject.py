a1bc60ad0d66e9f55af9a84fdb22fc40d3a9c0a1
ElijahAvril <eavril6@gmail.com>
2024-10-09 21:02:34 +0000

    Made the bird class, collision fix

--- finalproject.py
+++ finalproject.py
@@ -1,19 +1,26 @@
 #Authors: Bryan Soares & Elijah Avri
 
+
 import os
 import sys
 from pathlib import Path
+
+
+#Importing pygame
 import pygame
 pygame.init()
 
+#Calling path 
 FolderPath = os.path.abspath(sys.argv[0])
 RootPath = os.path.dirname(FolderPath)
+
+#Window screen setup 
 
 WindowScreen = pygame.display.set_mode((480,720))
 pygame.display.set_caption("Flappy Bird")
 
 
-# - - - - - - - - - - - -Image Class Setup - - - - - - - - - - - - - - - - - - - - -
+## - - - - - - - - - - - -Image Class Setup - - - - - - - - - - - - - - - - - - - - -
 
 LoadImage = os.path.join(RootPath, 'assets' , 'SkyAsset.png')
 Background = pygame.image.load(LoadImage)
@@ -31,12 +38,29 @@ BaseFloor = pygame.image.load(BaseImageLoad)
 
 
 
+# - - - - - - - - - - - - - - - - - - - Image Classes - - - - - - - - - - - - - - - - - - - - - - - -
 
+class Bird:
+   def __init__(self):
+        self.image = Player
+        self.x = 50  
+        self.y = 360  
+        self.gravity = 0.5
+        self.jump_strength = -10
+        self.velocity = 0
+    
+   def update(self):
+        self.velocity += self.gravity
+        self.y += self.velocity
+        if self.y > WindowScreen.get_height() - BaseFloor.get_height() - self.image.get_height():
+            self.y = WindowScreen.get_height() - BaseFloor.get_height() - self.image.get_height()
+            self.velocity = 0
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # - - - -Foor base position and speeed - - -
 base_speed = 2
 base_x = 0
 #- - - - - - - - - - - - - - - - - - - - - -
-
 
 running = True
 while running:
@@ -53,3 +77,4 @@ while running:
     WindowScreen.blit(Background, (0, 0))
     WindowScreen.blit(BaseFloor, (base_x, WindowScreen.get_height() - BaseFloor.get_height()))
     WindowScreen.blit(BaseFloor, (base_x + WindowScreen.get_width(), WindowScreen.get_height() - BaseFloor.get_height()))
+      
