c4232abf92480449218d2afc5b39e143129aeefe
ElijahAvril <eavril6@gmail.com>
2024-10-15 21:17:34 +0000

    Player class completed & keybinds

--- finalproject.py
+++ finalproject.py
@@ -41,20 +41,26 @@ BaseFloor = pygame.image.load(BaseImageLoad)
 # - - - - - - - - - - - - - - - - - - - Image Classes - - - - - - - - - - - - - - - - - - - - - - - -
 
 class Bird:
-   def __init__(self):
-        self.image = Player
+    def __init__(self):
+        self.image = pygame.transform.scale(Player, (50, 35))
         self.x = 50  
-        self.y = 360  
+        self.y = WindowScreen.get_height() / 2 - self.image.get_height() / 2
         self.gravity = 0.5
         self.jump_strength = -10
         self.velocity = 0
     
-   def update(self):
+    def update(self):
         self.velocity += self.gravity
         self.y += self.velocity
+        
         if self.y > WindowScreen.get_height() - BaseFloor.get_height() - self.image.get_height():
             self.y = WindowScreen.get_height() - BaseFloor.get_height() - self.image.get_height()
             self.velocity = 0
+            
+        if self.y < 0:
+            self.y = 0
+
+bird = Bird()
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # - - - -Foor base position and speeed - - -
@@ -64,17 +70,24 @@ base_x = 0
 
 running = True
 while running:
-    print("Running game")
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                bird.velocity = bird.jump_strength
+
+    bird.update()
 
     base_x -= base_speed
     if base_x <= -WindowScreen.get_width():
         base_x = 0
 
-    pygame.display.update()
     WindowScreen.blit(Background, (0, 0))
+    WindowScreen.blit(bird.image, (bird.x, bird.y))
     WindowScreen.blit(BaseFloor, (base_x, WindowScreen.get_height() - BaseFloor.get_height()))
     WindowScreen.blit(BaseFloor, (base_x + WindowScreen.get_width(), WindowScreen.get_height() - BaseFloor.get_height()))
-      
+
+    pygame.display.update()
+
+pygame.quit()
