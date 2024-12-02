63f7ec47404b4c5c98847c6bfa4a218dd11d31be
ElijahAvril <eavril6@gmail.com>
2024-12-02 02:16:28 +0000

    Inverted Pipe added/Pipes fixed

--- finalproject.py
+++ finalproject.py
@@ -45,7 +45,8 @@ BaseFloor = pygame.image.load(BaseImageLoad)
 
 # - - - - - - - - - - - - - - - - - - - Image/Assets setup - - - - - - - - - - - - - - - - - - - - - - - -
 
-class Bird:
+class Bird():
+    WIDTH = HEIGHT = 32
     def __init__(self): #Scales player image, sets the position and gravity, velocity, etc.
         self.image = pygame.transform.scale(Player, (50, 35))
         self.x = 50
@@ -69,12 +70,12 @@ class Bird:
             self.y = 0
     def check_for_collisions(self, pipes):
         for pipe in pipes:
-             if self.rect.colliderect(pipe.rect) == True:
-                return True
+             if   WindowScreen.blit(bird.image, (bird.x, bird.y)) == WindowScreen.blit(pipe.image, (pipe.x, pipe.y)) or WindowScreen.blit(bird.image, (bird.x, bird.y)) ==  WindowScreen.blit(pipe.image, (pipe.x, pipe.y + pipe.height + 500)):
+                return True 
         return False
     
 
-class Pipe: 
+class Pipe(): 
     def __init__(self, x, y):
         self.image = pygame.transform.scale(PipeImage, (250 , 350))
         self.x = x
@@ -90,10 +91,20 @@ class Pipe:
     def update(self):
         self.x -= self.speed
         if self.x < -self.width:
-            self.x = WindowScreen.get_width()
-            self.y = random.randint(-self.height, 0)
-            
-        
+            self.x = 450
+            self.y = 155
+            self.image = pygame.transform.scale(PipeImage, (250 , random.randint(400,650)))
+
+class InvertedPipe(Pipe):
+    def __init__(self, x, y):
+        super().__init__(x, y)
+        self.image = pygame.transform.flip(self.image,False, True)
+    def update(self):
+        self.x -= self.speed
+        if self.x < -self.width:
+            self.x = 450
+            self.y = 0
+            self.image = pygame.transform.scale(self.image, (250 , random.randint(350,650)))
 
 
 def game_over():
@@ -104,7 +115,8 @@ def game_over():
     pygame.time.delay(2000)
 
 bird = Bird() 
-pipes = [Pipe(300, -100), Pipe(600, -150)]
+pipe1 = Pipe(300, -100)
+pipe2 = InvertedPipe(300,0)
 
 
 
@@ -122,7 +134,7 @@ base_x = 1
 
 #Pygame application running 
 running = True
-while running:
+while running:  
     for event in pygame.event.get():
         if event.type == pygame.QUIT: 
             running = False
@@ -131,19 +143,19 @@ while running:
                 bird.velocity = bird.jump_strength
 
     bird.update() # Updates position of player
-           
-    if bird.check_for_collisions(pipes) == True:
-        game_over()
-        running = False
+    # if bird.y ==  WindowScreen.get_height() - BaseFloor.get_height() - bird.image.get_height():
+    #     game_over()
 
     base_x -= base_speed #Sets floor speed and position and moves it
     if base_x <= -WindowScreen.get_width():
         base_x = 0
 
-    
+   
 
-    for pipe in pipes:
-        pipe.update()
+    pipe1.update()
+    pipe2.update()
+        
+        
 
 #----- Setting Position of the background, player, and floor - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 
@@ -153,8 +165,8 @@ while running:
     WindowScreen.blit(BaseFloor, (base_x + WindowScreen.get_width(), WindowScreen.get_height() - BaseFloor.get_height()))
     WindowScreen.blit(BaseFloor, (base_x + 300,608.8)) #Overlap
     WindowScreen.blit(BaseFloor, (base_x + 700,608.8)) #Overlap
-    WindowScreen.blit(pipe.image, (pipe.x, pipe.y))
-    WindowScreen.blit(pipe.image, (pipe.x, pipe.y + pipe.height + 500)) 
+    WindowScreen.blit(pipe1.image, (pipe1.x, pipe1.y))
+    WindowScreen.blit(pipe2.image, (pipe2.x, pipe2.y))
     
 
 #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
